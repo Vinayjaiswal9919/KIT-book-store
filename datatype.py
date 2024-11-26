cdb541a4507d75ad490392d9e268b81b56f5f9ca
Vinayjaiswal99 <232792@kit.ac.in>
2024-11-26 12:00:30 +0530

    Added local files

---
+++ datatype.py
@@ -0,0 +1,56 @@
+# Numeric data data----------------------------------------------------------
+# 1 integer--
+a=5
+print("a is type:",type(a))
+b=4
+print(b)
+print()
+# 2 float--
+c=5.6
+print("c is type:",type(b))
+c=4
+print(c)
+print()
+# 3 complex--
+e=3+3j
+print( "e is type:",type(e), e)
+print()
+#sequenceing data type---------------------------------------------------------
+# 1 String--
+
+str1="ram"
+str2='vinay'
+print(str1,"type is",type(str1))
+print(str2*2)
+print()
+#2 list--
+a=[2,'ram',"vinay",3]
+print(a)
+print(a ,'is type ',type(a))
+print(a[2]*2)
+print()
+#3 Tuple--
+n=('ram',3,4,'5',"cat")
+print(n)
+print(n,"is type " ,type(n))
+print()
+#Dictionary data type---------------------------------------
+a={ 5:'ram', 'a':'cat','india':'New delhi'}
+print(a)
+print(a[5])
+print(a['a'])
+print()
+
+#Boolen data type--------------------------------------------
+a=True
+B=False
+print(a)
+print(a," is type of",type(a))
+print()
+
+#Set data type-------------------------------------------------
+a={'ram','2','cat','cat'}  #duplicate value not allow
+b={True,2,'ram' }
+print(a)
+print(b)
+
